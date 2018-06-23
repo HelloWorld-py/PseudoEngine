9cd1557e43f550417ae1cef84ea05b550cc7b134
HelloWorld-py <jdtsekrekos@live.com>
2018-06-23 18:14:29 -0400

    Added import

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # Dependencies: pynput, Pillow and all other associated package dependancies
 from .DeviceInput import *
 from .Graphics import Window
+from .utils.logger import Logger
 from . import enums
 from . import evf
 from .matrix import Matrix
